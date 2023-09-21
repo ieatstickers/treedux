1cd098a6a46d072f9e4c64b60999f37acd4f47bd
mikeburke <mikeburke.214@gmail.com>
2023-09-21 13:43:46 +0100

    - Tidy up

--- src/Type/RecursiveStateNode.ts
+++ src/Type/RecursiveStateNode.ts
@@ -1,4 +1,3 @@
-// Recursive type to generate the structure of your state tree with type-hinted methods
 import { MutatorCreators } from "./MutatorCreators";
 import { StateNodeWithMutators } from "./StateNodeWithMutators";
 import { OwnKeys } from "./OwnKeys";
@@ -6,7 +5,7 @@ import { MutatorMethods } from "./MutatorMethods";
 
 // Recursive type to represent a node in the state tree. It type-hints for the default methods available on every state
 // node to get, set, subscribe etc. as well as any custom mutator methods that have been passed in. It also type-hints
-// for any child nodes in the tree so you can continually traverse down.
+// for any child nodes in the tree, so you can continually traverse down.
 export type RecursiveStateNode<StateNodeType, StateInterface, StateNodeMutatorCreators extends MutatorCreators<StateNodeType, StateInterface> = {}> =
   StateNodeWithMutators<StateNodeType, MutatorMethods<StateInterface, StateNodeMutatorCreators>>
   & {
