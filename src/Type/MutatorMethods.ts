05442cb7dd41506845797fce2b2849b85c88f387
mikeburke <mikeburke.214@gmail.com>
2023-09-21 13:43:02 +0100

    - Fixed typo

--- src/Type/MutatorMethods.ts
+++ src/Type/MutatorMethods.ts
@@ -2,7 +2,7 @@ import { IsPOJO } from "./IsPojo";
 import { MutatorCreator } from "./MutatorCreator";
 import { MutatorCreators } from "./MutatorCreators";
 
-// Also it is MutatorCreators that are passed to a state node, it's actually the getAction() method of each mutator
+// Although it is MutatorCreators that are passed to a state node, it's actually the getAction() method of each mutator
 // that we want to type-hint for when traversing through a Treedux state tree. This utility type parses the MutatorCreators
 // and returns a type-hinted object of the getAction() methods for each mutator that can then be merged with the default
 // types for a state node
