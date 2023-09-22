b4031406c6d16ceead30ec22c39ad4f54faad2d1
mikeburke <mikeburke.214@gmail.com>
2023-09-22 20:31:03 +0100

    - Added example of automatically subscribing to state nodes in constructor

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,8 @@ import { AdblockStore } from "./Module/Adblock/AdblockStore";
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { useEffect, useState } from "react";
+import { StateNode } from "../src/Data/StateNode";
+import { RecursiveStateNode } from "../src/Type/RecursiveStateNode";
 
 // Init treedux
 const treedux = Treedux.init(
@@ -20,6 +22,125 @@ const treedux = Treedux.init(
     }
   }
 );
+
+type State<S> = {
+  [K in keyof S]?: S[K] | RecursiveStateNode<S[K], any>
+}
+
+abstract class AbstractComponent<P, S> extends React.Component<P, S>
+{
+  protected unsubscribers: { [key: string]: () => void } = {};
+  
+  protected initState(state: State<S>): S
+  {
+    const newState = state;
+    
+    for (const key in state)
+    {
+      const value = state[key];
+      
+      if (value instanceof StateNode)
+      {
+        // Subscribe to the state node
+        this.unsubscribers[`state_subscriber_${key}`] = value.subscribe((newValue) => {
+          this.setState({
+            [key]: newValue
+          } as S);
+        })
+        
+        // Get the current value
+        newState[key] = value.get();
+      }
+    }
+    
+    return newState as S;
+  }
+  
+  public componentWillUnmount()
+  {
+    for (const key in this.unsubscribers)
+    {
+      this.unsubscribers[key]();
+    }
+  }
+}
+
+interface ExampleComponentProps {}
+interface ExampleComponentState {
+  adblockStatus: string,
+  adblockWhitelist: string[],
+  user: {
+    name: string,
+    email: string
+  }
+}
+
+class ExampleComponent extends AbstractComponent<ExampleComponentProps, ExampleComponentState>
+{
+  public constructor(props: ExampleComponentProps)
+  {
+    super(props);
+    
+    this.state = this.initState({
+      adblockStatus: treedux.state.adblock.status,
+      adblockWhitelist: treedux.state.adblock.userSettings.whitelist,
+      user: treedux.state.user.user
+    })
+  }
+  
+  public render()
+  {
+    return <div>
+      <div>Name: {this.state.user?.name}</div>
+      <div>Email: {this.state.user?.email}</div>
+      
+      <button
+        onClick={() => {
+          console.log('clicked');
+          treedux
+            .state
+            .user
+            .user
+            .set({
+              name:  'Mr Test',
+              email: 'mr.test@test.com'
+            })
+            .dispatch();
+        }}
+      >
+        Click to Set User
+      </button>
+      
+      <button
+        onClick={() => {
+          console.log('clicked');
+          treedux
+            .state
+            .adblock
+            .userSettings
+            .whitelist
+            .add(`test-${Date.now()}.com`)
+            .dispatch();
+        }}
+      >
+        Add to Whitelist
+      </button>
+      
+      <div>
+        Whitelist:
+        
+        <ul>
+          {
+            this.state.adblockWhitelist.map((item, index) => <li key={index}>{item}</li>)
+          }
+        </ul>
+      </div>
+    
+    </div>
+  }
+}
+
+
 
 function Example() {
   
@@ -67,4 +188,4 @@ function Example() {
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<Example />);
+root.render(<ExampleComponent />);
