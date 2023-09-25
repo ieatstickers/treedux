b0763bcafcbece8ed3e7a39d5d4ec39d86eceaf7
mikeburke <mikeburke.214@gmail.com>
2023-09-25 21:54:55 +0100

    - Updated compiled files

--- dist/src/Data/AbstractMutator.d.ts
+++ dist/src/Data/AbstractMutator.d.ts
@@ -4,7 +4,7 @@ import { Action } from "./Action";
 import { Action as ReduxAction } from "redux";
 export declare abstract class AbstractMutator<State> implements MutatorInterface<State> {
     protected treedux: Treedux;
-    protected constructor(treedux: Treedux);
+    constructor(treedux: Treedux);
     abstract getType(): string;
     abstract getAction(...args: any): Action<any>;
     abstract reduce(state: State, action: ReduxAction): void;
