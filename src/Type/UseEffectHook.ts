135c9b79c583355f4b2636d0871c211d716eef81
mikeburke <mikeburke.214@gmail.com>
2023-10-02 16:46:59 +0100

    - Updated types for UseStateHook and UseEffectHook

--- src/Type/UseEffectHook.ts
+++ src/Type/UseEffectHook.ts
@@ -1,2 +1,7 @@
 
-export type UseEffectHook = (callback: () => void) => void;
+declare const UNIQUE_SYMBOL: unique symbol;
+type Destructor = () => void | { [UNIQUE_SYMBOL]: never };
+type EffectCallback = () => void | Destructor;
+type DependencyList = ReadonlyArray<any>;
+
+export type UseEffectHook = (effect: EffectCallback, deps?: DependencyList) => void;
