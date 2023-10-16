4dcca64cd911415f92177f0eee61d501641d8f4e
mikeburke <mikeburke.214@gmail.com>
2023-10-16 17:50:57 +0100

    - Improved support for object types

--- dist/src/Type/MutatorCreators.d.ts
+++ dist/src/Type/MutatorCreators.d.ts
@@ -4,7 +4,7 @@ import { OwnKeys } from "./OwnKeys";
 export type MutatorCreators<Type, StateInterface> = {
     [key: string]: MutatorCreator<StateInterface>;
 } & IsPOJO<Type> extends true ? {
-    [K in OwnKeys<Type>]?: MutatorCreators<Type[K], StateInterface>;
+    [K in OwnKeys<Type>]?: MutatorCreators<Type[K], StateInterface> | MutatorCreator<StateInterface>;
 } : {
     [key: string]: MutatorCreator<StateInterface>;
 };
