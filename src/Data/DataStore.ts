4300ad72a513678054ea755b76f6ba48cde2fc7b
mikeburke <mikeburke.214@gmail.com>
2024-06-04 11:24:57 +0100

    - Wrap bound reducer to avoid redux interfering with context

--- src/Data/DataStore.ts
+++ src/Data/DataStore.ts
@@ -71,7 +71,9 @@ export class DataStore<StateInterface, Mutators extends MutatorCreators<StateInt
       {
         if (reducerMap[key]) throw `Cannot add reducer. Action type already registered: ${key}`;
         const mutator = mutatorCreator(this.treedux);
-        reducerMap[mutator.getType()] = mutator.reduce.bind(mutator);
+        reducerMap[mutator.getType()] = (...args) => {
+          return mutator.reduce.call(mutator, ...args);
+        };
       }
     }
     
