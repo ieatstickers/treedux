7bf9feaf70427802b8309b6ebf87b29d0f84def6
mikeburke <mikeburke.214@gmail.com>
2025-02-04 15:15:44 +0000

    - Autoformat

--- src/Data/Action.ts
+++ src/Data/Action.ts
@@ -5,14 +5,14 @@ export class Action<Payload>
   private readonly treedux: Treedux;
   public readonly type: string;
   public readonly payload: Payload;
-
+  
   private constructor(action: { type: string, payload?: Payload }, treedux: Treedux)
   {
     this.type = action.type;
     this.payload = action.payload;
     this.treedux = treedux;
   }
-
+  
   public static create<Payload>(
     action: { type: string, payload?: Payload },
     treedux: Treedux
@@ -20,12 +20,12 @@ export class Action<Payload>
   {
     return new Action(action, treedux);
   }
-
+  
   public dispatch(): void
   {
     this.treedux.dispatch(this);
   }
-
+  
   public serialize(): { type: string, payload?: Payload }
   {
     return { type: this.type, payload: this.payload };
