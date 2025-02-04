53f7401f4e1da2d76c697ef85a1ded342c323296
mikeburke <mikeburke.214@gmail.com>
2025-02-04 15:18:15 +0000

    - Autoformat

--- src/Enum/DefaultActionEnum.ts
+++ src/Enum/DefaultActionEnum.ts
@@ -1,6 +1,6 @@
 export enum DefaultActionEnum
 {
-  BATCH = '__BATCH__',
-  SET_BY_KEY_PATH = '__SET_BY_KEY_PATH__',
-  DELETE_BY_KEY_PATH = '__DELETE_BY_KEY_PATH__',
+  BATCH = "__BATCH__",
+  SET_BY_KEY_PATH = "__SET_BY_KEY_PATH__",
+  DELETE_BY_KEY_PATH = "__DELETE_BY_KEY_PATH__",
 }
