3d75af1e529ed63c6aeaf4c641563ff18b705d2e
mikeburke <mikeburke.214@gmail.com>
2025-02-04 15:18:51 +0000

    - Autoformat

--- src/Type/IsPojo.ts
+++ src/Type/IsPojo.ts
@@ -1,4 +1,3 @@
-
 // Utility type to determine if a type is a plain old javascript object
 export type IsPOJO<T> = T extends Record<string, any>
   ? (T extends any[] | ((...args: any[]) => any) | Date | RegExp ? false : true)
