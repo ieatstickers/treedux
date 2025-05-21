5cb3753867c8ad4b6ed40386d5e7e221ee06d916
mikeburke <mikeburke.84@protonmail.com>
2025-05-21 17:57:20 +0100

    - Updated dist files

--- dist/src/Type/OwnKeys.d.ts
+++ dist/src/Type/OwnKeys.d.ts
@@ -1,3 +1,3 @@
 type DefaultKeys = keyof Object | keyof Array<any> | keyof Date | keyof RegExp | keyof string | keyof number | keyof boolean | keyof null | keyof undefined | keyof void | keyof symbol | keyof bigint;
-export type OwnKeys<T> = T extends null | undefined ? never : Exclude<keyof T, DefaultKeys>;
+export type OwnKeys<T> = Exclude<keyof T, DefaultKeys>;
 export {};
--- dist/src/Type/ReadOnlyRecursiveStateNode.d.ts
+++ dist/src/Type/ReadOnlyRecursiveStateNode.d.ts
@@ -3,5 +3,5 @@ import { IsPrimitive } from "./IsPrimitive";
 import { ReadOnlyStateNodeInterface } from "./ReadOnlyStateNodeInterface";
 import { ReadOnlyDynamicallyTraversable } from "./ReadOnlyDynamicallyTraversable";
 export type ReadOnlyRecursiveStateNode<StateNodeType, StateInterface> = ReadOnlyStateNodeInterface<StateNodeType> & (IsPrimitive<StateNodeType> extends true ? {} : ReadOnlyDynamicallyTraversable<StateNodeType, StateInterface>) & {
-    [K in OwnKeys<StateNodeType>]: ReadOnlyRecursiveStateNode<StateNodeType[K], StateInterface>;
+    [K in OwnKeys<NonNullable<StateNodeType>>]: ReadOnlyRecursiveStateNode<NonNullable<StateNodeType>[K], StateInterface>;
 };
