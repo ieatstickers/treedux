9b1f89238a3ea188c9540c88a05e5cb590e2aac1
mikeburke <mikeburke.84@protonmail.com>
2025-05-21 18:02:29 +0100

    - Updated dist files

--- dist/src/Type/ReadOnlyRecursiveStateNode.d.ts
+++ dist/src/Type/ReadOnlyRecursiveStateNode.d.ts
@@ -2,6 +2,6 @@ import { OwnKeys } from "./OwnKeys";
 import { IsPrimitive } from "./IsPrimitive";
 import { ReadOnlyStateNodeInterface } from "./ReadOnlyStateNodeInterface";
 import { ReadOnlyDynamicallyTraversable } from "./ReadOnlyDynamicallyTraversable";
-export type ReadOnlyRecursiveStateNode<StateNodeType, StateInterface> = ReadOnlyStateNodeInterface<StateNodeType> & (IsPrimitive<StateNodeType> extends true ? {} : ReadOnlyDynamicallyTraversable<StateNodeType, StateInterface>) & {
+export type ReadOnlyRecursiveStateNode<StateNodeType, StateInterface> = ReadOnlyStateNodeInterface<StateNodeType> & (IsPrimitive<StateNodeType> extends true ? {} : ReadOnlyDynamicallyTraversable<NonNullable<StateNodeType>, StateInterface>) & {
     [K in OwnKeys<NonNullable<StateNodeType>>]: ReadOnlyRecursiveStateNode<NonNullable<StateNodeType>[K], StateInterface>;
 };
