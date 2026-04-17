5488c5fa5fbae3346c9992661d610328f7143bec
mikeburke <mikeburke.84@protonmail.com>
2026-04-17 17:39:11 +0100

    - Added fast-deep-equal for more performant value comparisons in subscribers

--- src/Data/ReadOnlyStateNode.ts
+++ src/Data/ReadOnlyStateNode.ts
@@ -4,6 +4,7 @@ import { ObjectKeys } from "../Type/ObjectKeys";
 import { ObjectPropertyType } from "../Type/ObjectPropertyType";
 import { ReadOnlyRecursiveStateNode } from "../Type/ReadOnlyRecursiveStateNode";
 import { ReadOnlyStateNodeInterface } from "../Type/ReadOnlyStateNodeInterface";
+import equal from "fast-deep-equal";
 
 type ReadOnlyStateNodeOptions = {
   keyPath: Array<string>
@@ -14,7 +15,7 @@ export class ReadOnlyStateNode<StateNodeType, StateInterface> implements ReadOnl
   private readonly treedux: Treedux;
   private lastKnownValue: StateNodeType;
   private readonly keyPath: Array<string> = [];
-  
+
   protected constructor(
     options: ReadOnlyStateNodeOptions,
     treedux: Treedux
@@ -23,22 +24,22 @@ export class ReadOnlyStateNode<StateNodeType, StateInterface> implements ReadOnl
     this.treedux = treedux;
     this.keyPath = options.keyPath;
   }
-  
+
   public static create<StateNodeType, StateInterface>(options: ReadOnlyStateNodeOptions, treedux: Treedux): ReadOnlyRecursiveStateNode<StateNodeType, StateInterface>
   {
     return (new ReadOnlyStateNode<StateNodeType, StateInterface>(options, treedux)).createProxy();
   }
-  
+
   public get(): StateNodeType
   {
     const keys = [ ...this.keyPath ];
     let value = this.treedux.getState();
-    
+
     while (keys.length > 0)
     {
       // Get the next key
       const key = keys.shift();
-      
+
       // If there is a value for the key, use that
       if (Objects.isObject(value))
       {
@@ -50,27 +51,27 @@ export class ReadOnlyStateNode<StateNodeType, StateInterface> implements ReadOnl
         return this.lastKnownValue;
       }
     }
-    
+
     this.lastKnownValue = value;
     return this.lastKnownValue!;
   }
-  
+
   public subscribe(callback: (data: StateNodeType) => void): () => void
   {
     let currentValue = this.lastKnownValue;
-    
+
     return this.treedux.subscribe(() => {
       const newValue = this.get();
-      if (JSON.stringify(newValue) === JSON.stringify(currentValue)) return;
+      if (equal(newValue, currentValue)) return;
       currentValue = newValue;
       callback(currentValue);
     });
   }
-  
+
   public byKey<K extends ObjectKeys<StateNodeType>>(key: K): ReadOnlyRecursiveStateNode<ObjectPropertyType<StateNodeType, K>, StateInterface>
   {
     if (!key) throw `Key must be provided to byKey method.`;
-    
+
     return ReadOnlyStateNode.create(
       {
         keyPath: this.keyPath.concat([ key.toString() ])
@@ -78,17 +79,17 @@ export class ReadOnlyStateNode<StateNodeType, StateInterface> implements ReadOnl
       this.treedux
     ) as unknown as ReadOnlyRecursiveStateNode<ObjectPropertyType<StateNodeType, K>, StateInterface>;
   }
-  
+
   private createProxy(): ReadOnlyRecursiveStateNode<StateNodeType, StateInterface>
   {
     return new Proxy(this, {
       get(self, property: string | symbol)
       {
         if (typeof property !== "string") return null;
-        
+
         // If property is a default method, return it
         if (typeof self[property] === "function") return self[property].bind(self);
-        
+
         // Default to returning a new StateNode
         return ReadOnlyStateNode.create(
           {
@@ -99,5 +100,5 @@ export class ReadOnlyStateNode<StateNodeType, StateInterface> implements ReadOnl
       }
     }) as unknown as ReadOnlyRecursiveStateNode<StateNodeType, StateInterface>;
   }
-  
+
 }
--- src/Data/StateNode.ts
+++ src/Data/StateNode.ts
@@ -10,6 +10,7 @@ import { ObjectKeys } from "../Type/ObjectKeys";
 import { ObjectPropertyType } from "../Type/ObjectPropertyType";
 import { ReadOnlyRecursiveStateNode } from "../Type/ReadOnlyRecursiveStateNode";
 import { ReadOnlyStateNode } from "./ReadOnlyStateNode";
+import equal from "fast-deep-equal";
 
 type StateNodeOptions<T, StateInterface> = {
   keyPath: Array<string>,
@@ -22,7 +23,7 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
   private lastKnownValue: StateNodeType;
   private readonly keyPath: Array<string> = [];
   private readonly mutators: Options["mutators"];
-  
+
   protected constructor(
     options: StateNodeOptions<StateNodeType, StateInterface>,
     treedux: Treedux
@@ -32,22 +33,22 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
     this.keyPath = options.keyPath;
     this.mutators = options.mutators;
   }
-  
+
   public static create<StateNodeType, ParentStateNodeType, StateInterface, Options extends StateNodeOptions<StateNodeType, StateInterface> = StateNodeOptions<StateNodeType, StateInterface>>(options: StateNodeOptions<StateNodeType, StateInterface>, treedux: Treedux): RecursiveStateNode<StateNodeType, ParentStateNodeType, StateInterface, Options["mutators"]>
   {
     return (new StateNode<StateNodeType, ParentStateNodeType, StateInterface, Options>(options, treedux)).createProxy();
   }
-  
+
   public get(): StateNodeType
   {
     const keys = [ ...this.keyPath ];
     let value = this.treedux.getState();
-    
+
     while (keys.length > 0)
     {
       // Get the next key
       const key = keys.shift();
-      
+
       // If there is a value for the key, use that
       if (Objects.isObject(value) && value[key] !== undefined)
       {
@@ -59,11 +60,11 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
         return this.lastKnownValue;
       }
     }
-    
+
     this.lastKnownValue = value;
     return this.lastKnownValue;
   }
-  
+
   public set(value: StateNodeType): Action<{ keyPath: Array<string>, value: StateNodeType }>
   {
     return Action.create(
@@ -74,19 +75,19 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
       this.treedux
     );
   }
-  
+
   public subscribe(callback: (data: StateNodeType) => void): () => void
   {
     let currentValue = this.lastKnownValue;
-    
+
     return this.treedux.subscribe(() => {
       const newValue = this.get();
-      if (JSON.stringify(newValue) === JSON.stringify(currentValue)) return;
+      if (equal(newValue, currentValue)) return;
       currentValue = newValue;
       callback(currentValue);
     });
   }
-  
+
   public byKey<K extends ObjectKeys<StateNodeType>>(key: K): RecursiveStateNode<
     ObjectPropertyType<StateNodeType, K>, StateNodeType, StateInterface,
     K extends keyof Options["mutators"] ? Options["mutators"][K] extends MutatorCreators<ObjectPropertyType<StateNodeType, K>,
@@ -95,7 +96,7 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
   >
   {
     if (!key) throw `Key must be provided to byKey method.`;
-    
+
     return StateNode.create(
       {
         keyPath: this.keyPath.concat([ key.toString() ]),
@@ -109,7 +110,7 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
       K extends keyof Options["mutators"] ? Options["mutators"][K] extends MutatorCreators<ObjectPropertyType<StateNodeType, K>, StateInterface> ? Options["mutators"][K] : {} : {}
     >;
   }
-  
+
   public delete(): Action<{ keyPath: Array<string> }>
   {
     return Action.create(
@@ -120,27 +121,27 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
       this.treedux
     );
   }
-  
+
   public toReadOnly(): ReadOnlyRecursiveStateNode<StateNodeType, StateInterface>
   {
     return ReadOnlyStateNode.create({ keyPath: this.keyPath }, this.treedux) as unknown as ReadOnlyRecursiveStateNode<StateNodeType, StateInterface>;
   }
-  
+
   private createProxy(): RecursiveStateNode<StateNodeType, ParentStateNodeType, StateInterface, Options["mutators"]>
   {
     return new Proxy(this, {
       get(self, property: string | symbol)
       {
         if (typeof property !== "string") return null;
-        
+
         const mutatorMethod = self.getMutatorMethod(property);
-        
+
         // If mutator method exists, return it
         if (mutatorMethod) return mutatorMethod;
-        
+
         // If property is a default method, return it
         if (typeof self[property] === "function") return self[property].bind(self);
-        
+
         // Default to returning a new StateNode
         return StateNode.create(
           {
@@ -152,7 +153,7 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
       }
     }) as unknown as RecursiveStateNode<StateNodeType, ParentStateNodeType, StateInterface, Options["mutators"]>;
   }
-  
+
   private getMutatorMethod(methodName: string): MutatorInterface<any>["getAction"]
   {
     // If mutator method doesn't exist, return null
@@ -161,5 +162,5 @@ export class StateNode<StateNodeType, ParentStateNodeType, StateInterface, Optio
     const mutator = mutatorCreator(this.treedux);
     return mutator.getAction.bind(mutator);
   }
-  
+
 }
